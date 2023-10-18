6e5a8a3859e47bc39ae4a25771d6f5780a9353bd
kudrashova <j.kudrashova@gmail.com>
2023-10-18 14:35:49 +0200

    staging

--- models/staging/staging_weather.sql
+++ models/staging/staging_weather.sql
@@ -7,8 +7,8 @@ WITH temperature_daily AS (
          ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'totalsnow_cm')::VARCHAR)::FLOAT AS totalsnow_cm,
         ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'totalprecip_mm')::VARCHAR)::FLOAT AS totalprecip_mm,
         ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'avghumidity')::VARCHAR)::FLOAT AS avghumidity,
-        (extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'astro' -> 'sunrise')::VARCHAR AS sunrise, 
-        (extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'astro' -> 'sunset')::VARCHAR AS sunset,
+        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'astro' -> 'sunrise')::VARCHAR)::TIME AS sunrise, 
+        ((extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'astro' -> 'sunset')::VARCHAR)::TIME AS sunset,
         (extracted_data -> 'location' -> 'name')::VARCHAR  AS city,
         (extracted_data -> 'location' -> 'region')::VARCHAR  AS region,
         (extracted_data -> 'location' -> 'country')::VARCHAR  AS country,
@@ -17,24 +17,22 @@ WITH temperature_daily AS (
         (extracted_data -> 'forecast' -> 'forecastday' -> 0 -> 'day' -> 'condition' -> 'text')::VARCHAR AS condition
     FROM {{source("staging", "raw_temp")}}),
         temperatutre_daily_updated AS (
-            SELECT 
-                date,
-                substring(city, 2, (length(city)-2)) as city,
-                substring(region, 2, (length(region)-2)) as region,
-                substring(country, 2, (length(country)-2)) as country,
-                lat,
-                lon,
-                substring(sunrise, 2, (length(sunrise)-2)) as sunrise,
-                substring(sunset, 2, (length(sunset)-2)) as sunset,
-                maxtemp_c,
-                avgtemp_c,
-                mintemp_c,
-                maxwind_kph,
-                totalprecip_mm,
-                totalsnow_cm,
-                avghumidity,
-                substring(condition, 2, (length(condition)-2)) as condition
-            FROM temperature_daily
-)
-SELECT * 
+SELECT 
+    date,
+    substring(city, 2, (length(city)-2)) as city,
+    substring(region, 2, (length(region)-2)) as region,
+    substring(country, 2, (length(country)-2)) as country,
+    lat,
+    lon,
+    substring(sunrise, 2, (length(sunrise)-2)) as sunrise,
+    substring(sunset, 2, (length(sunset)-2)) as sunset,
+    maxtemp_c,
+    avgtemp_c,
+    mintemp_c,
+    maxwind_kph,
+    totalprecip_mm,
+    totalsnow_cm,
+    avghumidity,
+    substring(condition, 2, (length(condition)-2)) as condition
 FROM temperature_daily
+)
